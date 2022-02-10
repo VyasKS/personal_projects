2250f674d15e4f823189ff6b30037c508e5a0e35
VyasKS <vyaskanchinadham@gmail.com>
2022-02-10 14:58:27 +0100

    Refactored code & added documentation:

--- Computer Vision/image_segmentation/main.py
+++ Computer Vision/image_segmentation/main.py
@@ -1,12 +1,14 @@
 import data_preprocessing
 import transfer_learning
-
+import image_segmentation
 
 def main():
     # Prefer downloading from google images directly by uncommenting the 'download_google_images()' method
-    data_preprocessing
-    transfer_learning
+    # Below modules constitute the whole project pipeline
 
+    data_preprocessing          # Download data, clean & store in respective directories
+    transfer_learning           # Augment data, normalize, import resnet50, fine-tune, train for a few epochs, save model checkpoint
+    image_segmentation		# Trains DeepLabv3 model, segments & maps images, fine-tune model, validate & save final output
 
 if __name__ == '__main__':
     main()
